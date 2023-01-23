595d8f1fd5842542b8d19160713c75ea1a5c9172
Anirudh-rao <anirudh131299@gmail.com>
2023-01-23 17:18:37 +0530

    Update Variables2.rs

---
+++ 1.Variables/Variables2.rs
@@ -0,0 +1,14 @@
+// variables2.rs
+// Execute `rustlings hint variables2` or use the
+// `hint` watch subcommand for a hint.
+
+// I AM NOT DONE
+
+fn main() {
+    let x: _;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
